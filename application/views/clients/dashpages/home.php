c7cc8a200fc36a8ea7c0d9f623794184205c8a12
slurge <josias.txus.serna@gmail.com>
2020-11-26 21:01:24 -0600

    Agregado dashboard home de emergencia

--- application/views/clients/dashpages/home.php
+++ application/views/clients/dashpages/home.php
@@ -1,5 +1,5 @@
 <div class="p-2">
     <h1>Buen dia, <?= $userdata['name'] ?></h1>
-    <p><?php print_r($userdata) ?></p>
+    <img src="<?= site_url('assets/img/home.jpg') ?>" alt="" class="img-fluid">
     
 </div>
